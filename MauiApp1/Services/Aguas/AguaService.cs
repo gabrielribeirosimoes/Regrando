6e73040f96afeb6ea3cd0644378eafb5a412477c
gabrielribeirosimoes <gabrielribeirosimoes@hotmail.com>
2024-06-19 14:22:14 -0300

    alterado tela inicial

--- MauiApp1/Services/Aguas/AguaService.cs
+++ MauiApp1/Services/Aguas/AguaService.cs
@@ -11,7 +11,7 @@ namespace MauiApp1.Services.Aguas
     public class AguaService : Request
     {
         private readonly Request _request;
-        private const string apiUrlBase = "";
+        private const string apiUrlBase = "http://DBRegrando.mssql.somee.com/api/Aguas";
         private string _token;
 
         public AguaService(string token)
